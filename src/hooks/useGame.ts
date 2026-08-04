c18d78e39fa569404583ead6742e819225d6e80c
animariano <animariano55@gmail.com>
2026-08-04 00:11:36 -0300

    deck

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,25 +1,31 @@
 import { useState } from "react";
 
-import { createDeck } from "../services/DeckService";
+import { createDeck, shuffleDeck, drawCards } from "../services/DeckService";
 
 import type { GameState } from "../types/GameState";
 
 export function useGame() {
+  const playerDeck = shuffleDeck(createDeck());
+  const playerDraw = drawCards(playerDeck, 4);
+
+  const enemyDeck = shuffleDeck(createDeck());
+  const enemyDraw = drawCards(enemyDeck, 4);
+
   const [game] = useState<GameState>({
     turno: 1,
 
     player: {
       vida: 20,
-      mazo: createDeck(),
-      mano: createDeck(),
+      mazo: playerDraw.deck,
+      mano: playerDraw.hand,
       campo: [],
       descarte: [],
     },
 
     enemy: {
       vida: 20,
-      mazo: createDeck(),
-      mano: [],
+      mazo: enemyDraw.deck,
+      mano: enemyDraw.hand,
       campo: [],
       descarte: [],
     },
--- src/services/DeckService.ts
+++ src/services/DeckService.ts
@@ -1,7 +1,31 @@
 import { cards } from "../data/cards";
+import type { CardModel } from "../types/Card";
 
-console.log("Cartas cargadas:", cards);
-
-export function createDeck() {
+export function createDeck(): CardModel[] {
   return [...cards];
+}
+
+export function shuffleDeck(deck: CardModel[]): CardModel[] {
+  const shuffled = [...deck];
+
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+
+  return shuffled;
+}
+
+export function drawCards(
+  deck: CardModel[],
+  amount: number
+): {
+  hand: CardModel[];
+  deck: CardModel[];
+} {
+  return {
+    hand: deck.slice(0, amount),
+    deck: deck.slice(amount),
+  };
 }
